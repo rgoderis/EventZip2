4388a8dc0684a4260cdbcd88ef1afde883a27598
Ryan Goderis <rgoderis789@gmail.com>
2020-04-22 15:15:07 -0400

    created components of Header and ParallaxContainer, imported both components in app.js file.

---
+++ client/src/Components/ParallaxContainer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ParallaxContainer extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Parallax Container</h1>
+          {/*  Weather Display */}
+          {/* API Buttons */}
+          {/* Zip Dropdown */}
+          {/* Results Container */}
+            {/* Result Card */}
+            </div>
+        )
+    }
+}
+
+export default ParallaxContainer
